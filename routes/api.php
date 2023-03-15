a2eca576d4a0357c17dbbc3e95cae0f11f521e3b
ipal <72238450+petelpop@users.noreply.github.com>
2023-03-15 10:56:56 +0700

    show id user

---
+++ routes/api.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\PostController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+
+
+Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
+Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
+Route::get('/posts2/{id}', [PostController::class, 'show2']);
+Route::post('/login', [AuthenticationController::class, 'login']);
