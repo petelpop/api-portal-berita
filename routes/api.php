ca78927b364aa45eba6ff7a3af0673d8b1e30337
ipal <72238450+petelpop@users.noreply.github.com>
2023-03-16 10:54:50 +0700

    Create Post

--- routes/api.php
+++ routes/api.php
@@ -16,10 +16,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 
+Route::middleware(['auth:sanctum'])->group(function (){
+  Route::get('/posts', [PostController::class, 'index']);
+  Route::get('/posts/{id}', [PostController::class, 'show']);
+  Route::post('/posts', [PostController::class, 'store']);
+  Route::get('/logout', [AuthenticationController::class, 'logout']);
+  Route::get('/me', [AuthenticationController::class, 'me']);
+});
 
-Route::get('/posts', [PostController::class, 'index'])->middleware(['auth:sanctum']);
-Route::get('/posts/{id}', [PostController::class, 'show'])->middleware(['auth:sanctum']);
 Route::get('/posts2/{id}', [PostController::class, 'show2']);
 Route::post('/login', [AuthenticationController::class, 'login']);
-Route::get('/logout', [AuthenticationController::class, 'logout'])->middleware('auth:sanctum');
-Route::get('/me', [AuthenticationController::class, 'me'])->middleware('auth:sanctum');
