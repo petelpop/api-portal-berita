131772d59ceeb4f3b279949844e37f0ee6bd2a88
ipal <72238450+petelpop@users.noreply.github.com>
2023-03-16 11:06:47 +0700

    delete method activated

--- routes/api.php
+++ routes/api.php
@@ -21,6 +21,7 @@ Route::middleware(['auth:sanctum'])->group(function (){
   Route::get('/posts/{id}', [PostController::class, 'show']);
   Route::post('/posts', [PostController::class, 'store']);
   Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware(['post.owner']);
+  Route::delete('/posts/{id}', [PostController::class, 'delete'])->middleware(['post.owner']);
   
   Route::get('/logout', [AuthenticationController::class, 'logout']);
   Route::get('/me', [AuthenticationController::class, 'me']);
