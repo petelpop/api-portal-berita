6c6699300dd5867c05f913523419c89c859feed7
ipal <72238450+petelpop@users.noreply.github.com>
2023-03-16 13:24:34 +0700

    Create Comment Route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\CommentController;
 use App\Http\Controllers\PostController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -23,6 +24,8 @@ Route::middleware(['auth:sanctum'])->group(function (){
   Route::patch('/posts/{id}', [PostController::class, 'update'])->middleware(['post.owner']);
   Route::delete('/posts/{id}', [PostController::class, 'delete'])->middleware(['post.owner']);
   
+  Route::post('/comment', [CommentController::class, 'store']);
+
   Route::get('/logout', [AuthenticationController::class, 'logout']);
   Route::get('/me', [AuthenticationController::class, 'me']);
 });
